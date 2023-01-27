a6ccea1b3ab65667cbb4d46ff8fa13d04410d193
jahime001 <jahimecamea@gmail.com>
2023-01-27 10:27:10 -0500

    changed resume link

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -9,29 +9,67 @@ function Nav() {
 
 
     return (
-        <div className='nav'>
-            <div className="logo">
-                <h1>Jahime's</h1>
-                <h3>Portfolio</h3>
-            </div>
-           
-            <div className="nav-links">
-                <Link to="home" spy={true} smooth={true} offset={-100} duration={500}>Home</Link>
-                <Link to="about" spy={true} smooth={true} offset={-100} duration={500}>About Me</Link>
-                <Link to="skills" spy={true} smooth={true} offset={-100} duration={500}>Skills</Link>
-                <Link to='showcase' spy={true} smooth={true} offset={-100} duration={500}>Project Showcase</Link>
-                <Link to='contact' spy={true} smooth={true} offset={-100} duration={500}>Contact Info</Link>
-                <a href="https://docs.google.com/document/d/16rDeAAa-y5AFHv2ngGRZWnroX6hTYRioxZUy8RzuQtc/edit?usp=sharing" target='blank'>Resume</a>
-            </div>
-
-
-            <main>
-                <Routes>
-                    <Route path='/about' element={<About />} />
-                </Routes>
-            </main>
+      <div className="nav">
+        <div className="logo">
+          <h1>Jahime's</h1>
+          <h3>Portfolio</h3>
         </div>
-    )
+
+        <div className="nav-links">
+          <Link to="home" spy={true} smooth={true} offset={-100} duration={500}>
+            Home
+          </Link>
+          <Link
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            About Me
+          </Link>
+          <Link
+            to="skills"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Skills
+          </Link>
+          <Link
+            to="showcase"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Project Showcase
+          </Link>
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Contact Info
+          </Link>
+          <a
+            href="https://docs.google.com/document/d/1X6AwbSkeWyjvvCpZT2Lh8B0fnFPgzK8yCIj8b8hKAsQ/edit?usp=sharing"
+            target="blank"
+          >
+            Resume
+          </a>
+        </div>
+
+        <main>
+          <Routes>
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </div>
+    );
 }
 
 export default Nav
