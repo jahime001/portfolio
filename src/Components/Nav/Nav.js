def988607c46181e7ab9880318c84f4f3d8bc774
jahime001 <jahimecamea@gmail.com>
2023-03-16 08:37:34 -0400

    side nav for mobile

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 import { Route, Routes } from "react-router-dom";
 import About from "../About/About";
 import { Link } from "react-scroll";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { TfiMenu, TfiClose } from "react-icons/tfi";
 
 function Nav() {
-
   let currentWidth = window.innerWidth;
+  const [showNav, setShowNav] = useState(false);
+  const handleShowNavbar = () => {
+    setShowNav(!showNav);
+  };
 
   if (currentWidth > 600) {
     return (
@@ -70,12 +74,6 @@ function Nav() {
             Contact Info
             <div class="underline"></div>
           </Link>
-          {/* <a
-            href="https://docs.google.com/document/d/1X6AwbSkeWyjvvCpZT2Lh8B0fnFPgzK8yCIj8b8hKAsQ/edit?usp=sharing"
-            target="blank"
-          >
-            Resume
-          </a> */}
         </div>
 
         <main>
@@ -93,15 +91,88 @@ function Nav() {
           <h3>Portfolio</h3>
         </div>
 
-        <div className="nav-links">
-          <a href="https://www.linkedin.com/in/jahimecameau/" target="blank">
+        <div className="nav-opener">
+          {/* <a href="https://www.linkedin.com/in/jahimecameau/" target="blank">
             <AiFillLinkedin className="floater-nav" />
           </a>
           <a href="https://github.com/jahime001" target="blank">
             <AiFillGithub className="floater-nav" />
-          </a>
+          </a> */}
+          {showNav === false ? (
+            <TfiMenu className="nav-opener-icon" onClick={handleShowNavbar} />
+          ) : (
+            <TfiClose
+              className="nav-opener-icon"
+              onClick={handleShowNavbar}
+              style={{ color: "black" }}
+            />
+          )}
         </div>
-
+        <div className={`side-blur ${showNav && "active"}`}></div>
+        <div className={`sidenav ${showNav && "active"}`}>
+          <div className="nav-links-ham">
+            <Link
+              activeClass="active"
+              onClick={handleShowNavbar}
+              to="home"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              Home
+              <div class="underline"></div>
+            </Link>
+            <Link
+              activeClass="active"
+              onClick={handleShowNavbar}
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              About Me
+              <div class="underline"></div>
+            </Link>
+            <Link
+              activeClass="active"
+              onClick={handleShowNavbar}
+              to="skills"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              Skills
+              <div class="underline"></div>
+            </Link>
+            <Link
+              activeClass="active"
+              onClick={handleShowNavbar}
+              to="showcase"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              Projects
+              <div class="underline"></div>
+            </Link>
+            <Link
+              activeClass="active"
+              onClick={handleShowNavbar}
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              Contact Info
+              <div class="underline"></div>
+            </Link>
+          </div>
+        </div>
         <main>
           <Routes>
             <Route path="/about" element={<About />} />
